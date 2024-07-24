ccd927bfb29a5a19fd56a53ac6c38eea3f126b95
erika-lee-site <169239308+erika-lee-site@users.noreply.github.com>
2024-07-24 16:14:17 -0700

    backend clerk

--- clerk-test/backend/server.js
+++ clerk-test/backend/server.js
@@ -2,11 +2,57 @@ const { ClerkExpressRequireAuth, ClerkExpressWithAuth } = require('@clerk/clerk-
 const cors = require('cors')
 const express = require('express')
 require('dotenv').config()
+const { Webhook } = require('svix')
+
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(express.json())
 app.use(cors());
+
+
+// see https://docs.svix.com/receiving/verifying-payloads/how#nodejs-express
+app.post(
+  '/api/webhooks',
+  bodyParser.raw({type: 'application/json'}), (req, res) => {
+      // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+      const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+
+      if (!WEBHOOK_SECRET) {
+        throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+      }
+
+      console.log(req.headers)
+      const payload = req.body;
+      const headers = req.headers;
+  
+      const wh = new Webhook(WEBHOOK_SECRET);
+      let msg;
+      try {
+          msg = wh.verify(payload, headers);
+      } catch (err) {
+        console.log(err)
+        res.status(400);
+      }
+
+      // Do something with the payload
+      // For this guide, you simply log the payload to the console
+      const { id } = msg.data;
+      const eventType = msg.type;
+      console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
+      console.log('Webhook body:', req.body)
+      if (eventType === 'user.updated') {
+        console.log('userId:', msg.data)
+      }
+
+      res.status(200)
+  }
+);
+
+// this has to be defined after the webhook
+// https://stackoverflow.com/questions/76281195/stripe-webhook-payload-must-be-provided-as-a-string-or-a-buffer
+app.use(express.json());
+
 
 // Use the strict middleware that raises an error when unauthenticated
 app.get(
@@ -33,7 +79,6 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(401).send('Unauthenticated!');
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
